417d7d59d3ba4561e92bc02d2956504df83f5ce0
yousufjoy <yousufj93006@gmail.com>
2023-10-30 10:53:58 +0600

    update

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useContext } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import CounterPage from './src/pages/CounterPage';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -6,37 +7,52 @@ import { NavigationContainer } from '@react-navigation/native';
 import LoginPage from './src/pages/LoginPage';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import SettingsPage from './src/pages/SettingsPage';
-
+import { AuthContext, AuthProvider } from './src/providers/AuthProvider';
 
 const stack = createStackNavigator();
-
 const bottom_tab = createBottomTabNavigator();
 
-const BasicDashboardScreen = ()=>{
-  return(
+const BasicDashboardScreen = () => {
+  return (
+    <stack.Navigator>
+      <stack.Screen name="CounterPage" component={CounterPage} />
+    </stack.Navigator>
+  );
+};
 
-  <stack.Navigator >
-    <stack.Screen name="CounterPage" component={CounterPage} />
-    <stack.Screen name="LoginPage" component={LoginPage} />
-  </stack.Navigator>)
-  
-}
+const LoginScreen = () => {
+  return (
+    <stack.Navigator>
+      <stack.Screen name="LoginPage" component={LoginPage} />
+    </stack.Navigator>
+  );
+};
+
+const DashboardScreen = () => {
+  return (
+    <bottom_tab.Navigator>
+      <bottom_tab.Screen name="Dashboard" component={BasicDashboardScreen} />
+      <bottom_tab.Screen name="Settings" component={SettingsPage} />
+    </bottom_tab.Navigator>
+  );
+};
 
 export default function App() {
   return (
-    <NavigationContainer>
-
-  <bottom_tab.Navigator>
-     
-        <bottom_tab.Screen name="Dashboard" component={BasicDashboardScreen} />
-        <bottom_tab.Screen name="LoginPage" component={LoginPage} />
-
-     
-  </bottom_tab.Navigator>
-    
-    </NavigationContainer>
-   
+    <AuthProvider>
+      <AuthContext.Consumer>
+        {(auth) =>
+          auth?.isLoggedIn ? (
+            <NavigationContainer>
+              <DashboardScreen />
+            </NavigationContainer>
+          ) : (
+            <NavigationContainer>
+              <LoginScreen />
+            </NavigationContainer>
+          )
+        }
+      </AuthContext.Consumer>
+    </AuthProvider>
   );
 }
-
-
---
+++ src/providers/AuthProvider.tsx
@@ -0,0 +1,24 @@
+
+
+import React, { useState } from 'react';
+
+export type IAuthContext = {
+    userName : String,
+    setUserName : (userName : string) => void,
+    isLoggedIn: boolean, 
+    setIsLoggedIn: (userName: boolean) => void;
+}
+
+const AuthContext = React.createContext<IAuthContext | null > (null)
+const AuthProvider = (props:any) => {
+    const [userName, setUserName] = useState("")
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    return (
+        <AuthContext.Provider value={{userName, setUserName, isLoggedIn, setIsLoggedIn}}>
+            {props.children}
+        </AuthContext.Provider>
+    )
+}
+
+export {AuthContext, AuthProvider}
